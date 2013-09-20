2575a52be4e35e58f65d5479767834f719fb618b
Nicholas <i.am@aweso.ms>
2013-09-20 09:21:34 -0400

    Add JQuery loader and external script list

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -1,3 +1,18 @@
+// Load external libraries
+
+if (true) {
+  var script_list = ['https://raw.github.com/Icehawk78/FrozenCookies/master/fc_button.js']
+  var jquery = document.createElement('script');
+  jquery.setAttribute('type', 'text/javascript');
+  jquery.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+  jquery.onload = function() {
+    scipt_list.forEach(function(url){
+      $.getScript(url);
+    });
+  };
+  document.head.appendChild(jquery);
+}
+
 // Global Variables
 
 var autoBuy = true;
