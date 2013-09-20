a71df4168820ef2b7ff1bc5ede68e1ae120cd996
Nicholas <i.am@aweso.ms>
2013-09-20 00:44:56 -0400

    Extract Menu/Button code

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -1,24 +1,8 @@
 javascript:(
-	function () {
-		var js = document.createElement('script');
-		js.setAttribute('type', 'text/javascript');
-		js.setAttribute('src', 'https://github.com/Icehawk78/FrozenCookies/blob/master/frozen_cookies.js');
-     	document.head.appendChild(js);
-    
-		var frozenCookieButton = document.createElement('div');
-		frozenCookieButton.setAttribute('id', 'fcButton');
-		frozenCookieButton.setAttribute('class', 'button');
-		frozenCookieButton.setAttribute('style', 'font-size: 60%;');
-		frozenCookieButton.innerHTML = "Frozen Cookie";
-		document.getElementById('comments').insertBefore(frozenCookieButton, document.getElementById('logButton'));
-
-		var frozenCookieCSS = document.createElement('style');
-		frozenCookieCSS.setAttribute('type', 'text/css');
-		frozenCookieCSS.innerHTML = '#fcButton { font-size: 60%; top: 0px; right: -16px; padding-top: 14px; padding-right: 16px; padding-bottom: 10px; padding-left: 0px; } #fcButton:hover { right: -8px; }';
-
-		document.getElementsByTagName('head')[0].appendChild(frozenCookieCSS);
-
-		l('fcButton').onclick=function(){Game.ShowMenu('fcMenu');};
-   
-   }()
+  function () {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('src', 'https://github.com/Icehawk78/FrozenCookies/blob/master/frozen_cookies.js');
+    document.head.appendChild(js);
+ }()
 );
