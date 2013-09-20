cc3acbba73725e8d518a458296c4357fbf22431d
Nicholas <i.am@aweso.ms>
2013-09-20 10:15:06 -0400

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -1,12 +1,17 @@
-var fcButton = document.createElement('div');
 $('#logButton').before(
-  $(fcButton).addClass('button').addClass('fcButton')
+  $(fcButton).addClass('button')
     .attr('id', 'fcButton')
     .html('Frozen Cookie')
     .click(function(){
       Game.ShowMenu('fc_menu');
     })
 );
+
+$('<style type="text/css">')
+  .html(
+  '#fcButton {font-size: 60%; top: 0px; right: -16px; padding: 14px 16px 10px 0px;}' +
+  '#fcButton:hover {right: -8px;}')
+  .appendTo('head');
 
 Game.oldUpdateMenu = Game.UpdateMenu;
 
