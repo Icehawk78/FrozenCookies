51ef12f1e3bec4d6e06bd03b4b19bcd4557499fd
Nicholas <i.am@aweso.ms>
2013-09-20 15:02:50 -0400

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -20,14 +20,14 @@ function updateTimers() {
     strokeStyle: '#CCC',
     strokeWidth: 10,
     x: 50, y: 50,
-    radius: 50
+    radius: 40
   });
-  fill_amount = Game.goldenCookie.delay / (1200 * Game.fps)
+  fill_amount = Game.goldenCookie.delay / (450 * Game.fps)
   canvas.drawArc({
     strokeStyle: '#FFF',
     strokeWidth: 10,
     x: 50, y: 50,
-    radius: 50,
+    radius: 40,
     start: 0,
     end: 180 * fill_amount
   })
