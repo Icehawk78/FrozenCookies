7387dbf27602cba29602d332287b37d022a8462d
Nicholas <i.am@aweso.ms>
2013-09-20 10:06:58 -0400

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -6,11 +6,12 @@ if (true) {
   jquery.setAttribute('type', 'text/javascript');
   jquery.setAttribute('src', 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
   jquery.onload = function() {
+    $.get('https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.css', function(data) {
+      $('<style type="text/css" />').html(data).appendTo('head');
+    });
     script_list.forEach(function(url){
       $.getScript(url);
     });
-    var css = $('<link rel="stylesheet" type="text/css" />').appendTo('head');
-    css.attr('href', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.css');
   };
   document.head.appendChild(jquery);
 }
