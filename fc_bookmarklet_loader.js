56863e6f12a7572764221488477155e12a0268d8
Nicholas <i.am@aweso.ms>
2013-09-24 14:49:19 -0400

    Update fc_bookmarklet_loader.js

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -2,7 +2,7 @@ javascript:(
   function () {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/Saeldur/frozen_cookies.js');
+    js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
     document.head.appendChild(js);
  }()
 );
