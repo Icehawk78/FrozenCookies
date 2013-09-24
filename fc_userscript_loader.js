2c842230be10c6e3865272d4ef8fe96558431a29
Robert Pendell <shinji@elite-systems.org>
2013-09-24 15:52:36 -0400

    Update fc_userscript_loader.js

--- fc_userscript_loader.js
+++ fc_userscript_loader.js
@@ -14,5 +14,7 @@ function LoadFrozenCookies() {
   js.setAttribute('type', 'text/javascript');
   js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
   document.head.appendChild(js);
- }
- window.addEventListener("load", LoadFrozenCookies, false);
+}
+// It's not the best way but Chrome doesn't work with addEventListener... :(
+// Delay load by 2 seconds to allow the site to load itself first.)
+window.setTimeout(LoadFrozenCookies, 2000);
