483d9a8bed9d8a9962cda2d0d819c06a713cd447
Nicholas <i.am@aweso.ms>
2013-10-08 13:03:41 -0400

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -144,7 +144,7 @@ function drawCircles(t_d, canvas) {
   var c = canvas;
   var i_c = 0;
   var t_b = ['#AAA','#BBB','#CCC','#DDD','#EEE','#FFF'];
-  var maxWidth = Math.max.apply(null,t_d.map(function(o){return (o.name && o.display) ? c.measureText({font: "10px Arial", text: o.name + ': ' + o.display}).width : 250;}));
+  var maxWidth = Math.max.apply(null,t_d.map(function(o){return (o.name && o.display) ? c.measureText({font: "10px Arial", maxWidth:c.width, text: o.name + ': ' + o.display}).width : 250;}));
   c.drawRect({
     fillStyle: '#999',
     x: 225, y: 12.5+t_d.length/2*15,
