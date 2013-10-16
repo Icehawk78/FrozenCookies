6ec1674ecde9c4532ba43c4786bdccdd2c960b2a
Nicholas <i.am@aweso.ms>
2013-10-16 14:37:36 -0400

    Remove tooltips... maybe?

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -90,10 +90,12 @@ function setOverrides() {
   eval("Game.Draw = " + Game.Draw.toString()
     .replace(/if \(Game.cookies>=me.price\) l\('product'\+me.id\).className='product enabled'; else l\('product'\+me.id\).className='product disabled';/, '(Game.cookies >= me.price) ? $("#product"+me.id).addClass("enabled").removeClass("disabled") : $("#product"+me.id).addClass("disabled").removeClass("enabled");')
     .replace(/if \(Game.cookies>=me.basePrice\) l\('upgrade'\+i\).className='crate upgrade enabled'; else l\('upgrade'\+i\).className='crate upgrade disabled';/, '(Game.cookies >= me.basePrice) ? $("#upgrade"+me.id).addClass("enabled").removeClass("disabled") : $("#upgrade"+me.id).addClass("disabled").removeClass("enabled");'));
+/*
   Game.RebuildStore=function(recalculate) {rebuildStore(recalculate);}
   Game.RebuildUpgrades=function(recalculate) {rebuildUpgrades(recalculate);}
   Game.RebuildStore(true);
   Game.RebuildUpgrades(true);
+*/
 }
 
 function preferenceParse(setting, defaultVal) {
