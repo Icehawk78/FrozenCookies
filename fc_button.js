33be6ff82902d0be8ed1d897173e3e5f6cb0fdf4
Nicholas <i.am@aweso.ms>
2013-10-15 23:24:21 -0400

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -108,7 +108,7 @@ function updateTimers() {
     });
   }
   if (bankMax > 0) {
-    var maxColor = (Game.cookies < bankTotal) ? 'rgba(252, 212, 0, 1)' : 'rgba(201, 169, 0, 1)'
+    var maxColor = (Game.cookies >= bankTotal) ? 'rgba(252, 212, 0, 1)' : 'rgba(201, 169, 0, 1)'
     t_draw.push({
       f_percent: bankMax,
       name: "Max Bank",
