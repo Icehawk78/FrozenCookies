d599e954f3695679c5f39d161bd7737a9ddab29e
Nicholas Laux <i.am@aweso.ms>
2013-11-15 12:52:43 -0500

    Merge changes from master

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -1,7 +1,7 @@
 // Global Variables
 var FrozenCookies = {
-  'baseUrl': 'https://raw.github.com/Icehawk78/FrozenCookies/master',
-  'branch' : 'M',
+  'baseUrl': 'https://raw.github.com/Icehawk78/FrozenCookies/development',
+  'branch' : 'D',
   'version': 1.03934
 };
 
