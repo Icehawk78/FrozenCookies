695a104f379c835e5e8e82c126977a8cfb4a84bc
Nicholas <i.am@aweso.ms>
2013-11-17 02:53:36 -0500

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -113,7 +113,7 @@ function formatEveryThirdPower(notations) {
 }
 
 function scientificNotation(value) {
-  if (value === 0 || (Math.abs(value) > 1 && Math.abs(value) < 100) {
+  if (value === 0 || (Math.abs(value) > 1 && Math.abs(value) < 100)) {
     return Math.round(value * 1000) / 1000;
   }
   var sign = value > 0 ? '' : '-';
