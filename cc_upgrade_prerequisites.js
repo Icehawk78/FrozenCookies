c23f40fa86470fc0195557732a83cffe53439cf6
Icehawk78 <i.am@aweso.ms>
2014-03-08 14:50:26 -0500

    Prereq typo

--- cc_upgrade_prerequisites.js
+++ cc_upgrade_prerequisites.js
@@ -166,7 +166,7 @@ var upgradeJson = [
   {'id':132,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[131]},
   {'id':133,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[132]},
   
-  {'id':152,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[182],
+  {'id':152,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[182]},
   {'id':153,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[152],'santa':1},
   {'id':154,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[152],'santa':1},
   {'id':155,'buildings':[0,0,0,0,0,0,0,0,0,0,0],'upgrades':[152],'santa':1},
