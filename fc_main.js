f557945e2f22b398ba9e3f82cd62d9145a363618
Nicholas Laux <i.am@aweso.ms>
2014-03-11 15:22:23 -0400

    Cost reduction, take three point one

--- fc_main.js
+++ fc_main.js
@@ -707,7 +707,7 @@ function upgradeStats(recalculate) {
         Game.elderWrath = existingWrath;
         var deltaCps = cpsNew - cpsOrig;
         var baseDeltaCps = baseCpsNew - baseCpsOrig;
-        var efficiency = (priceReductionTest > cost) ? 0 : purchaseEfficiency(cost, deltaCps, baseDeltaCps, cpsOrig)
+        var efficiency = (priceReductionTest > cost) ? 1 : purchaseEfficiency(cost, deltaCps, baseDeltaCps, cpsOrig)
         return {'id' : current.id, 'efficiency' : efficiency, 'base_delta_cps' : baseDeltaCps, 'delta_cps' : deltaCps, 'cost' : cost, 'purchase' : current, 'type' : 'upgrade'};
       }
     }).filter(function(a){return a;});
