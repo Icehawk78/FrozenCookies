4e4a576429e017049c080c67707e00f619232d63
Nicholas <i.am@aweso.ms>
2014-04-12 15:20:25 -0400

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -648,7 +648,7 @@ function haveAll(holiday) {
 function checkPrices(currentUpgrade) {
   var value = 0;
   if (FrozenCookies.caches.recommendationList.length > 0) {
-    var nextRec = FrozenCookies.caches.recommendationList.filter(function(i){return i.id != currentUpgrade.id)[0];
+    var nextRec = FrozenCookies.caches.recommendationList.filter(function(i){return i.id != currentUpgrade.id;})[0];
     var nextPrereq = (nextRec.type == 'upgrade') ? unfinishedUpgradePrereqs(nextRec.purchase) : null;
     nextRec = (nextPrereq == null || nextPrereq.filter(function(u){return u.cost != null;}).length == 0) ? nextRec : FrozenCookies.caches.recommendationList.filter(function(a){return nextPrereq.some(function(b){return b.id == a.id && b.type == a.type})})[0];
     value = nextRec.cost == null ? 0 : (nextRec.cost / totalDiscount(nextRec.type == 'building')) - nextRec.cost;
