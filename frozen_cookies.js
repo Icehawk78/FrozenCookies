3b8d09919dc3975f4baad4121a388c8d59642bbe
Nicholas <i.am@aweso.ms>
2014-05-20 16:18:33 -0400

    Version Tick

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -6,7 +6,7 @@ var scriptElement = document.getElementById( 'frozenCookieScript' ),
 	FrozenCookies = {
 		'baseUrl': baseUrl,
 		'branch' : 'G',
-		'version': 1.2
+		'version': 1.3
 	};
 
 // Load external libraries
