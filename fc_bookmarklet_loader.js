f0d1a153e927742198ad109a088af4e1a937d785
Nicholas <i.am@aweso.ms>
2014-08-25 17:50:50 -0400

    Beta Version Tick

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -3,7 +3,7 @@ javascript:(
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('id', 'frozenCookieScript');
-    js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
+    js.setAttribute('src', 'https://rawgithub.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
     document.head.appendChild(js);
  }()
 );
