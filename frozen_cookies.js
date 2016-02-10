b65c3d2c0605a3272e0f80260a4179a3044272bb
Nicholas <i.am@aweso.ms>
2016-02-09 22:29:57 -0500

    Add findIndex polyfill

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -1,3 +1,27 @@
+// Polyfill findIndex for older browsers
+if (!Array.prototype.findIndex) {
+  Array.prototype.findIndex = function(predicate) {
+    if (this === null) {
+      throw new TypeError('Array.prototype.findIndex called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
 // Global Variables
 var scriptElement = document.getElementById( 'frozenCookieScript' );
 var version_list = [1.0466, 1.0501, 1.9, 1.905];
