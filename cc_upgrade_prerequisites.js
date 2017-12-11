9262c48c95e4de8baaef7e013073c423d972693c
Chevron42 <34353764+Chevron42@users.noreply.github.com>
2017-12-11 13:38:41 -0500

    typo

--- cc_upgrade_prerequisites.js
+++ cc_upgrade_prerequisites.js
@@ -87,7 +87,8 @@ FrozenCookies.preferenceValues = {
     'showAchievements':{
         'hint':'Show achievement popups',
         'display':['Achievement Popups OFF','Achievement Popups ON'],
-        'default':0,
+        'default':0
+    },
     'numberDisplay':{
         'hint':'Change how numbers are shortened',
         'display':["Raw Numbers","Full Word (million, billion)","Initials (M, B)","SI Units (M, G, T)", "Scientific Notation (6.3e12)"],
