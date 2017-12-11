20ae5f83dc8f3eb6ea0d87e9f4a9baefbab9a3fe
Chevron42 <34353764+Chevron42@users.noreply.github.com>
2017-12-11 13:35:35 -0500

    Added displaying achievement popups

--- fc_main.js
+++ fc_main.js
@@ -1571,6 +1571,8 @@ function fcWin(what) {
                 if (!FrozenCookies.disabledPopups) {
                     logEvent('Achievement', 'Achievement unlocked :<br>' + Game.Achievements[what].name + '<br> ', true);
                 }
+                if (FrozenCookies.showAchievements) {
+                    Game.Notify('Achievement unlocked','<div class="title" style="font-size:18px;margin-top:-2px;">'+name+'</div>',Game.Achievements[what].icon);
                 if (Game.Achievements[what].pool != 'shadow') {
                     Game.AchievementsOwned++;
                 }
