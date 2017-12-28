fe5868d56ba94587cbc206d8fe0e00a4e8e45623
Chevron42 <34353764+Chevron42@users.noreply.github.com>
2017-12-28 14:29:41 -0500

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -1881,7 +1881,7 @@ function autoCookie() {
         
         if (mostExpensive == 0) mostExpensive = 4.1300226e40;
         
-        if (FrozenCookies.autoBuy && (Game.cookies >= mostExpensive || !(FrozenCookies.AutoSpell == 3)) && (Game.cookies >= delay + recommendation.cost) && (FrozenCookies.pastemode || isFinite(nextChainedPurchase().efficiency))) {
+        if (FrozenCookies.autoBuy && (Game.cookies >= mostExpensive || !(FrozenCookies.autoSpell == 3)) && (Game.cookies >= delay + recommendation.cost) && (FrozenCookies.pastemode || isFinite(nextChainedPurchase().efficiency))) {
             //    if (FrozenCookies.autoBuy && (Game.cookies >= delay + recommendation.cost)) {
             recommendation.time = Date.now() - Game.startDate;
             //      full_history.push(recommendation);  // Probably leaky, maybe laggy?
