acdc11002735232ff0939e114c525bd7d848bbf5
kevmert <kevmert@yahoo.com>
2018-02-24 16:24:18 -0800

    Update frozen_cookies.js

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -4,7 +4,7 @@ var scriptElement = document.getElementById('frozenCookieScript') !== null ?
     document.getElementById('modscript_frozen_cookies');
 var baseUrl = scriptElement !== null ?
     scriptElement.getAttribute('src').replace(/\/frozen_cookies\.js$/, '') :
-    'https://rawgit.com/Lordshinjo/FrozenCookies/master';
+    'https://rawgit.com/Chevron42/FrozenCookies/master';
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
