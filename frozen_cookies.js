b20b519eec224e64c1d0ae5f74385fa76267df6e
hnns88 <31962615+hnns88@users.noreply.github.com>
2018-08-04 11:21:54 +0200

    Update URL

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -4,7 +4,7 @@ var scriptElement = document.getElementById('frozenCookieScript') !== null ?
     document.getElementById('modscript_frozen_cookies');
 var baseUrl = scriptElement !== null ?
     scriptElement.getAttribute('src').replace(/\/frozen_cookies\.js$/, '') :
-    'https://rawgit.com/Chevron42/FrozenCookies/master';
+    'https://rawgit.com/hnns88/FrozenCookies/master';
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
