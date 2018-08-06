3b0df0349b5b5c5a0cd63b3b9cae1551e10d71db
hnns88 <31962615+hnns88@users.noreply.github.com>
2018-08-06 22:10:07 +0200

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -298,7 +298,7 @@ function updateTimers() {
         maxColor = (Game.cookies >= bankTotal) ? 'rgba(252, 212, 0, 1)' : 'rgba(201, 169, 0, 1)';
         t_draw.push({
             f_percent: bankMax,
-            name: !FrozenCookies.setHarvestBank ? "Max Bank" : "Harvest Bank",
+            name: !FrozenCookies.setHarvestBankPlant ? "Max Bank" : "Harvest Bank",
             display: Beautify(bankTotal),
             c1: maxColor,
             overlay: true
