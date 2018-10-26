2821c3a14512a27ae8db03072e5774cf93a86222
Mtarnuhal <info@luukdebresser.nl>
2018-10-26 02:39:29 +0200

    Update fc_userscript_loader.user.js

--- fc_userscript_loader.user.js
+++ fc_userscript_loader.user.js
@@ -5,8 +5,8 @@
 // @author         Lordshinjo
 // @homepage       https://github.com/Mtarnuhal/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      https://rawgit.com/Mtarnuhal/FrozenCookies/master/fc_userscript_loader.js
-// @downloadURL    https://rawgit.com/Mtarnuhal/FrozenCookies/master/fc_userscript_loader.js
+// @updateURL      https://cdn.jsdelivr.net/gh/Mtarnuhal/FrozenCookies@9994735c95fe6e0dd0122eed9382b137fe6150c4/fc_userscript_loader.user.js
+// @downloadURL    https://cdn.jsdelivr.net/gh/Mtarnuhal/FrozenCookies@9994735c95fe6e0dd0122eed9382b137fe6150c4/fc_userscript_loader.user.js
 // @run-at         document-start
 // ==/UserScript==
 
@@ -15,7 +15,7 @@
 // Github.io: http://lordshinjo.github.io/FrozenCookies/
 
 function LoadFrozenCookies() {
-    Game.LoadMod('https://rawgit.com/Mtarnuhal/FrozenCookies/master/frozen_cookies.js');
+    Game.LoadMod('https://cdn.jsdelivr.net/gh/Mtarnuhal/FrozenCookies@9994735c95fe6e0dd0122eed9382b137fe6150c4/frozen_cookies.js');
 }
 
 window.addEventListener("load", LoadFrozenCookies, false);
