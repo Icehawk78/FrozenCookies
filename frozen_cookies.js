fcf0e906092c9a037e77047ed907d928ba3aef87
Mtarnuhal <info@luukdebresser.nl>
2018-10-28 01:40:39 +0200

    Updated the version number
    Since that never happened.

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -8,7 +8,7 @@ var baseUrl = scriptElement !== null ?
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
-    'version': '1.6.0'
+    'version': '1.7.0'
 };
 
 // Load external libraries
