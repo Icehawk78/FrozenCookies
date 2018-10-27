455243af56ecd717d3c71a344d142fe1ca897ac6
Mtarnuhal <info@luukdebresser.nl>
2018-10-28 01:17:31 +0200

    Update fc_bookmarklet_loader.js

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -1,5 +1,5 @@
 javascript:(
     function () {
-        Game.LoadMod('https://cdn.jsdelivr.net/gh/Mtarnuhal/FrozenCookies@9994735c95fe6e0dd0122eed9382b137fe6150c4/frozen_cookies.js');
+        Game.LoadMod('https://mtarnuhal.github.io/FrozenCookies/frozen_cookies.js');
     }()
 );
