94767cdcc1203dbc744ae1068474327518b0dc2a
Gregory King <greg@flyingpawn.com>
2020-09-12 14:07:09 -0400

    Update fc_main.js
    fix autobuy?

--- fc_main.js
+++ fc_main.js
@@ -2174,6 +2174,22 @@ function autoGSBuy() {
     }
 }
 
+function safeBuy(bldg,count)
+{
+	var wasSell = false;
+	
+	if (Game.buyMode==-1) 
+	{
+		wasSell = true;
+		document.getElementById('storeBulkBuy').click();
+		bldg.buy(count);
+		document.getElementById('storeBulkSell').click();
+	} else {
+		bldg.buy(count);
+	}
+	
+}
+
 function autoGodzamokAction()
 {
     if (!T) return; //Just leave if Pantheon isn't here yet
@@ -2194,12 +2210,14 @@ function autoGodzamokAction()
 		
         if ((FrozenCookies.autoGodzamok >= 1) && Game.Objects['Cursor'].amount < 10) 
 		{
-			Game.Objects['Cursor'].buy(count);
+			//Game.Objects['Cursor'].buy(count);
+			safeBuy(Game.Objects['Cursor'],count);
 		}
 		
         if ((FrozenCookies.autoGodzamok >= 1) && Game.Objects['Farm'].amount < 10) 
 		{
-			Game.Objects['Farm'].buy(count2);
+			//Game.Objects['Farm'].buy(count2);
+			safeBuy(Game.Objects['Farm'],count2);
 		}
     }
 }
@@ -2307,8 +2325,9 @@ function autoCookie() {
             recommendation.purchase.clickFunction = null;
             disabledPopups = false;
             //      console.log(purchase.name + ': ' + Beautify(recommendation.efficiency) + ',' + Beautify(recommendation.delta_cps));
-            recommendation.purchase.buy();
-            FrozenCookies.autobuyCount += 1;
+            //recommendation.purchase.buy();
+            if (recommendation.type=='upgrade') {recommendation.purchase.buy();} else {safeBuy(recommendation.purchase);} //safebuy building
+			FrozenCookies.autobuyCount += 1;
             if (FrozenCookies.trackStats == 5 && recommendation.type == 'upgrade') {
                 saveStats();
             } else if (FrozenCookies.trackStats == 6) {
