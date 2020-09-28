18ddc3e201a591ef073b3bcb4256d6630c9c6e5e
Aaron Wright <aaron@infocreations.com>
2020-09-28 01:03:19 -0600

    Cleanup preferences (#40)
    * move preference values into their own file
    
    * rearrange preferences into sections
    
    * reword prefs for consistency
    
    Reword the preference text and the values both for readability and consistency with the existing Cookie Clicker preferences.
    
    * fix typo

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -26,6 +26,7 @@ var script_list = [
     '//cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/plugins/jqplot.highlighter.min.js',
     '//cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/plugins/jqplot.logAxisRenderer.min.js',
     '//cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/plugins/jqplot.cursor.min.js',
+    FrozenCookies.baseUrl + '/fc_preferences.js',
     FrozenCookies.baseUrl + '/cc_upgrade_prerequisites.js',
     FrozenCookies.baseUrl + '/fc_main.js',
     FrozenCookies.baseUrl + '/fc_button.js',
